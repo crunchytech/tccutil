2e6ded3b491d04cc5f58f0f17d0c1b27ea88322d
colossus689 <colossus689@users.noreply.github.com>
2022-07-08 19:08:44 -0400

    python3 macOS 12 compatible

--- tccutil.py
+++ tccutil.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # ****************************************************************************
 # tccutil.py, Utility to modify the macOS Accessibility Database (TCC.db)
